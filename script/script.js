9b1f14395a2d9573e617086039f02b857e94b513
issei416 <mukeshkj2912@gmail.com>
2024-06-26 20:34:53 +0530

    added README and rectified edge cases

--- script/script.js
+++ script/script.js
@@ -1,23 +1,26 @@
-
+// defining headers for API request
 let myHeaders = new Headers();
 myHeaders.append("apikey", "DTu7I8ygdE6WzKgAy86ETD46BghoSTYw");
 
+
+// request options for API call
 let getrequestOptions = {
     method: 'GET',
     redirect: 'follow',
     headers: myHeaders
 };
 
+// getting the input elements using DOM
 let to = document.getElementById("to");
 let from = document.getElementById("from");
 let amount = document.getElementById("amount");
-
-
 let fromamount = document.getElementById("cfrom");
 let camount = document.getElementById("camount");
 
+// url to get all available country codes and currency symbols
 let symbolurl = "https://api.apilayer.com/exchangerates_data/symbols";
 
+// async function to fetch symbols and codes
 async function getsymbols(url, requestoptions) {
     try {
         let response = await fetch(url, requestoptions);
@@ -28,6 +31,8 @@ async function getsymbols(url, requestoptions) {
         window.alert("Oops something went wrong: " + error.message);
     }
 }
+
+// appending the symbols as options for input elements after fetching
 getsymbols(symbolurl,getrequestOptions).then(data => {
     let symbols = data.symbols;
     for(key in symbols){
@@ -42,15 +47,19 @@ getsymbols(symbolurl,getrequestOptions).then(data => {
     }
 })
 
+//selecting submit button
 let btn = document.getElementById("btn");
-btn.addEventListener('click', () => {
+btn.addEventListener('click', () => { // eventlistener for submit btn on click
+    //selecting elements to display the conversion
     let convertto = to.value;
     console.log(convertto);
     let convertfrom = from.value;
     console.log(convertfrom);
     let convertamount = amount.value;
     console.log(convertamount);
+    //url to get call conversion API call
     let url = `https://api.apilayer.com/exchangerates_data/convert?to=${convertto}&from=${convertfrom}&amount=${convertamount}`
+    //async function to get conversion from the selected inputs
     async function convert(url, requestoptions) {
         try {
             let response = await fetch(url, requestoptions);
@@ -61,6 +70,7 @@ btn.addEventListener('click', () => {
             window.alert("Oops something went wrong: " + error.message);
         }
     }
+    //appending the conversion data to the display field
     convert(url,getrequestOptions).then(data => {
         fromamount.innerText = convertamount
         camount.innerText = Number(data.result).toFixed(2);
